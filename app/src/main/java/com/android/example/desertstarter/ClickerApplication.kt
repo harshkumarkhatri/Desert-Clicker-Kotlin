734eee2533a3347776f84b4e9bddcc0fbed477bc
harshkumarkhatri <mailharshkhatri@gmail.com>
2020-05-23 20:15:49 +0530

    Completed the logging and the app

---
+++ app/src/main/java/com/android/example/desertstarter/ClickerApplication.kt
@@ -0,0 +1,11 @@
+package com.android.example.desertstarter
+
+import android.app.Application
+import timber.log.Timber
+
+class ClickerApplication:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
